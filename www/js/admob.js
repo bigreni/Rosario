eca486d3e830b565de2b25ccb125305f2a2377ab
bigreni <bigreni@gmail.com>
2017-01-09 21:11:00 -0600

    Version 4

--- www/js/admob.js
+++ www/js/admob.js
@@ -121,25 +121,25 @@
 
    function checkFirstUse()
     {
-        var currentVersion = 2;
-        var p = window.localStorage.getItem("currentVersion");
-        if (p == null) //App downloaded first time
-        {
-//Spanish
-            navigator.notification.alert('Para ver el menú del teléfono, por favor, arrastra la pantalla desde arriba hacia abajo o desde abajo hacia arriba.', initApp, 'Gracias por descargar la app', 'OK');
-            window.localStorage.setItem("currentVersion", currentVersion);
-        }
-        else if(p < currentVersion) //if app upgraded
-        {
-//Spanish
-            navigator.notification.alert('Para ver el menú del teléfono, por favor, arrastra la pantalla desde arriba hacia abajo o desde abajo hacia arriba.', initApp, 'Gracias por actualizar la app', 'OK');
-            window.localStorage.setItem("currentVersion", currentVersion);            
-        }
-        else
-        {
+//        var currentVersion = 2;
+//        var p = window.localStorage.getItem("currentVersion");
+//        if (p == null) //App downloaded first time
+//        {
+////Spanish
+//            navigator.notification.alert('Para ver el menú del teléfono, por favor, arrastra la pantalla desde arriba hacia abajo o desde abajo hacia arriba.', initApp, 'Gracias por descargar la app', 'OK');
+//            window.localStorage.setItem("currentVersion", currentVersion);
+//        }
+//        else if(p < currentVersion) //if app upgraded
+//        {
+////Spanish
+//            navigator.notification.alert('Para ver el menú del teléfono, por favor, arrastra la pantalla desde arriba hacia abajo o desde abajo hacia arriba.', initApp, 'Gracias por actualizar la app', 'OK');
+//            window.localStorage.setItem("currentVersion", currentVersion);            
+//        }
+//        else
+//        {
             askRating();
             initApp();
-        }
+//        }
     }
 
 function askRating()

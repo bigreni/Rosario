7a4c2b5559ecb8cde1607e15bb3069573a632af0
bigreni <bigreni@gmail.com>
2017-04-04 21:24:15 -0500

    Version 6

--- www/js/admob.js
+++ www/js/admob.js
@@ -10,7 +10,7 @@
     if (/(android)/i.test(navigator.userAgent)) {
         admobid = { // for Android
             banner: 'ca-app-pub-1683858134373419/8619965489',
-            interstitial:'ca-app-pub-1683858134373419/8648803882'
+            interstitial:'ca-app-pub-9249695405712287/8883612750'
             //banner: 'ca-app-pub-3886850395157773/3411786244'
             //interstitial: 'ca-app-pub-9249695405712287/3301233156'
         };
